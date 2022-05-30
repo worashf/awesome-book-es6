b656153a49d9647f203a83aef346f2ed0211bcc6
worashf <uppert83@gmail.com>
2022-05-31 00:19:17 +0300

    add selector

---
+++ modules/Selector.js
@@ -0,0 +1,13 @@
+export const listBook = document.getElementById('book-list');
+export const addForm = document.getElementById('add-form');
+export const bookListing = document.getElementById('listBook');
+export const bookReg = document.getElementById('addBook');
+export const contact = document.getElementById('contact-section');
+
+export const listLink = document.getElementById('list-book');
+export const addLink = document.getElementById('new-book');
+export const contactLink = document.getElementById('contact');
+export const date = document.getElementById('date');
+
+export const title = document.getElementById('title');
+export const author = document.getElementById('author');
